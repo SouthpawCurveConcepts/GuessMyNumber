09ef25c721d55f065e825c6cd703bee3068cd54d
Jay For Short <jagoblacksky@gmail.com>
2021-07-28 02:14:34 -0500

    75. Manipulating CSS Styles

--- starter/script.js
+++ starter/script.js
@@ -19,25 +19,36 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess, ', ' + guess, 'click');
 
+  // No guess entered
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
     score--;
     document.querySelector('.score').textContent = score;
     console.log(typeof guess, guess, 'not', 'score', score);
+
+    // Guess is correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
     console.log(typeof guess, guess, 'correct', 'score', score);
+
+    // Guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High!';
       score--;
       document.querySelector('.score').textContent = score;
       console.log(typeof guess, guess, 'high', 'score', score);
+
+      // Player loses game
     } else {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low!';
